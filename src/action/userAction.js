91b47a42e59b3cfe1fe28603500dc2a69993c7d8
minsol <kth083030@naver.com>
2024-06-01 17:03:30 +0900

    login test !

--- src/action/userAction.js
+++ src/action/userAction.js
@@ -2,9 +2,40 @@ import api from '../utils/api';
 import * as types from '../constants/user.constants';
 import { commonUiActions } from './commonUiAction';
 import * as commonTypes from '../constants/commonUI.constants';
-const loginWithToken = () => async (dispatch) => {};
-const loginWithEmail = (payload) => async (dispatch) => {};
-const logout = () => async (dispatch) => {};
+const loginWithToken = () => async (dispatch) => {
+  try {
+    dispatch({ type: types.LOGIN_WITH_TOKEN_REQUEST });
+    const response = await api.get('/user/me');
+    if (response.status !== 200) throw new Error(response.error);
+    console.log('rrr', response);
+    dispatch({
+      type: types.LOGIN_WITH_TOKEN_SUCCESS,
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({ type: types.LOGIN_WITH_TOKEN_FAIL });
+    dispatch(logout());
+  }
+};
+const loginWithEmail =
+  ({ email, password }) =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: types.LOGIN_REQUEST });
+      const response = await api.post('/auth/login', { email, password });
+      if (response.status !== 200) throw new Error(response.error);
+      sessionStorage.setItem('token', response.data.token);
+      dispatch({ type: types.LOGIN_SUCCESS, payload: response.data });
+    } catch (error) {
+      dispatch({ type: types.LOGIN_FAIL, payload: error.error });
+    }
+  };
+const logout = () => async (dispatch) => {
+  // user 정보 지우고
+  dispatch({ type: types.LOGOUT });
+  // 세션 토큰 값도 지우자
+  sessionStorage.removeItem('token');
+};
 
 const loginWithGoogle = (token) => async (dispatch) => {};
 
@@ -22,11 +53,13 @@ const registerUser =
           'success',
         ),
       );
+      dispatch({ type: types.RESET_ERROR }); // 에러 상태 초기화
       navigate('/login');
     } catch (error) {
       dispatch({ type: types.REGISTER_USER_FAIL, payload: error.error });
     }
   };
+
 export const userActions = {
   loginWithToken,
   loginWithEmail,
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,30 +1,32 @@
-import React, { useState } from "react";
-import { Container, Form, Button, Alert } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { userActions } from "../action/userAction";
+import React, { useState } from 'react';
+import { Container, Form, Button, Alert } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { userActions } from '../action/userAction';
 
-import "../style/login.style.css";
+import '../style/login.style.css';
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.user);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const error = useSelector((state) => state.user.error);
 
   const loginWithEmail = (event) => {
     event.preventDefault();
     //이메일,패스워드를 가지고 백엔드로 보내기
+    dispatch(userActions.loginWithEmail({ email, password }));
   };
 
   const handleGoogleLogin = async (googleData) => {
     // 구글로 로그인 하기
   };
 
+  // 이미 로그인한 유저 로그인페이지 이동 방지
   if (user) {
-    navigate("/");
+    navigate('/');
   }
   return (
     <>
@@ -59,7 +61,7 @@ const Login = () => {
               Login
             </Button>
             <div>
-              아직 계정이 없으세요?<Link to="/register">회원가입 하기</Link>{" "}
+              아직 계정이 없으세요?<Link to="/register">회원가입 하기</Link>{' '}
             </div>
           </div>
 

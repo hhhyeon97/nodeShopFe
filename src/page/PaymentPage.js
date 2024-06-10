d0ee5049de536908dd5dca9d318ab48bbe24634b
minsol <kth083030@naver.com>
2024-06-11 00:44:58 +0900

    오더생성1차

--- src/page/PaymentPage.js
+++ src/page/PaymentPage.js
@@ -30,13 +30,31 @@ const PaymentPage = () => {
     city: '',
     zip: '',
   });
+
+  const { cartList, totalPrice } = useSelector((state) => state.cart);
   // console.log('shipinfo', shipInfo);
 
   //맨처음 페이지 로딩할때는 넘어가고  오더번호를 받으면 성공페이지로 넘어가기
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    //오더 생성하가ㅣ
+    const { firstName, lastName, contact, address, city, zip } = shipInfo;
+    // console.log('cartList',cartList);
+    const data = {
+      totalPrice,
+      shipTo: { address, city, zip },
+      contact: { firstName, lastName, contact },
+      orderList: cartList.map((item) => {
+        return {
+          productId: item.productId._id,
+          price: item.productId.price,
+          qty: item.qty,
+          size: item.size,
+        };
+      }),
+    };
+    //오더 생성하기
+    dispatch(orderActions.createOrder(data));
   };
 
   const handleFormChange = (event) => {
@@ -47,12 +65,22 @@ const PaymentPage = () => {
 
   const handlePaymentInfoChange = (event) => {
     //카드정보 넣어주기
+    const { name, value } = event.target;
+    if (name === 'expiry') {
+      let newValue = cc_expires_format(value);
+      setCardValue({ ...cardValue, [name]: newValue });
+      return;
+    }
+    setCardValue({ ...cardValue, [name]: value });
   };
 
   const handleInputFocus = (e) => {
     setCardValue({ ...cardValue, focus: e.target.name });
   };
   //카트에 아이템이 없다면 다시 카트페이지로 돌아가기 (결제할 아이템이 없으니 결제페이지로 가면 안됌)
+  if (cartList.length === 0) {
+    navigate('/cart');
+  }
   return (
     <Container>
       <Row>
@@ -123,7 +151,7 @@ const PaymentPage = () => {
                   </Form.Group>
                 </Row>
                 <div className="mobile-receipt-area">
-                  {/* <OrderReceipt /> */}
+                  <OrderReceipt cartList={cartList} totalPrice={totalPrice} />
                 </div>
                 <div>
                   <h2 className="payment-title">결제 정보</h2>
@@ -146,7 +174,7 @@ const PaymentPage = () => {
           </div>
         </Col>
         <Col lg={5} className="receipt-area">
-          {/* <OrderReceipt /> */}
+          <OrderReceipt cartList={cartList} totalPrice={totalPrice} />
         </Col>
       </Row>
     </Container>
